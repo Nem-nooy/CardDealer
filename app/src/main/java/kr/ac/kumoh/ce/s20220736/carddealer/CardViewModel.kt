8b682a0b9631ba00cf40cdcd4f33fb28cfbca8f5
Nemnooy <sgu101310@gmail.com>
2024-11-28 23:54:26 +0900

    🃏 포커 족보 판별

--- app/src/main/java/kr/ac/kumoh/ce/s20220736/carddealer/CardViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20220736/carddealer/CardViewModel.kt
@@ -7,17 +7,53 @@ import kotlin.random.Random
 
 class CardViewModel : ViewModel() {
     private val _cards = MutableLiveData<IntArray>(IntArray(5) { 0 })
-
     val cards: LiveData<IntArray>
         get() = _cards
 
+    private val _handRank = MutableLiveData<String>()
+    val handRank: LiveData<String>
+        get() = _handRank
+
     fun shuffle() {
         val newCards = mutableSetOf<Int>()
-
         while (newCards.size < 5) {
             newCards.add(Random.nextInt(52))
         }
-
         _cards.value = newCards.sorted().toIntArray()
+        evaluateHandRank()
+    }
+
+    private fun evaluateHandRank() {
+        val cards = _cards.value ?: return
+
+        // 카드 숫자와 무늬를 분리
+        val numbers = cards.map { it % 13 }
+        val suits = cards.map { it / 13 }
+
+        // 숫자별 개수 세기
+        val numberCounts = numbers.groupingBy { it }.eachCount()
+
+        // 무늬별 개수 세기
+        val suitCounts = suits.groupingBy { it }.eachCount()
+
+        val isFlush = suitCounts.size == 1
+        val sortedNumbers = numbers.sorted()
+        val isStraight = sortedNumbers.zipWithNext().all { (a, b) -> b == a + 1 } ||
+                (sortedNumbers == listOf(0, 9, 10, 11, 12)) // A,10,J,Q,K
+
+        val rank = when {
+            isFlush && isStraight && sortedNumbers.contains(12) -> "로열 스트레이트 플러시"
+            isFlush && isStraight -> "스트레이트 플러시"
+            4 in numberCounts.values -> "포카드"
+            numberCounts.values.containsAll(listOf(3, 2)) -> "풀하우스"
+            isFlush -> "플러시"
+            isStraight -> "스트레이트"
+            3 in numberCounts.values -> "쓰리카드"
+            numberCounts.values.count { it == 2 } == 2 -> "투페어"
+            2 in numberCounts.values -> "원페어"
+            else -> "하이카드"
+        }
+
+        _handRank.value = rank
     }
 }
--- app/src/main/java/kr/ac/kumoh/ce/s20220736/carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20220736/carddealer/MainActivity.kt
@@ -1,8 +1,10 @@
+// MainActivity.kt
 package kr.ac.kumoh.ce.s20220736.carddealer
 
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,7 +12,6 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.s20220736.carddealer.databinding.ActivityMainBinding
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
@@ -18,10 +19,9 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("DiscouragedApi")     // '쓰면 안 좋은 것 알지만, 그럼에도 쓰겠다'는 의지 표명
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         Log.i("Lifecycle is started!!!!!!!", "onCreate()")
         enableEdgeToEdge()
-        // setContentView(R.layout.activity_main)
+
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
@@ -37,8 +37,6 @@ class MainActivity : AppCompatActivity() {
 
         model.cards.observe(this, Observer {
             // cards 값이 바뀌면 옵저버 발동
-            // val res = IntArray(5)
-
             model.cards.value!!.forEachIndexed { index, num ->
                 imgCards[index].setImageResource(
                     resources.getIdentifier(
@@ -48,32 +46,14 @@ class MainActivity : AppCompatActivity() {
                     )
                 )
             }
+        })
 
-            // mainBinding.imgCard1.setImageResource(res[0])
-            // mainBinding.imgCard2.setImageResource(res[1])
-            // mainBinding.imgCard3.setImageResource(res[2])
-            // mainBinding.imgCard4.setImageResource(res[3])
-            // mainBinding.imgCard5.setImageResource(res[4])
+        model.handRank.observe(this, Observer { rank ->
+            Toast.makeText(this, "당신의 족보는 $rank 입니다.", Toast.LENGTH_SHORT).show()
         })
 
         mainBinding.btnDeal.setOnClickListener {
             model.shuffle()
-
-            // 위에서 옵저버 걸어놨기 때문에, 이제 아래는 필요 X
-//            model.cards.value!!.forEachIndexed { index, num ->
-//                res[index] = resources.getIdentifier(
-//                    getCardName(num),
-//                    "drawable",
-//                    packageName
-//                )
-//            }
-//
-//            mainBinding.imgCard1.setImageResource((res[0]))
-//            mainBinding.imgCard2.setImageResource((res[1]))
-//            mainBinding.imgCard3.setImageResource((res[2]))
-//            mainBinding.imgCard4.setImageResource((res[3]))
-//            mainBinding.imgCard5.setImageResource((res[4]))
-
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -91,56 +71,17 @@ class MainActivity : AppCompatActivity() {
             3 -> "clubs"
             else -> "error"
         }
-
         val number = when (c % 13) {
             0 -> "ace"
-            in 1 .. 9 -> (c % 13 + 1).toString()
+            in 1..9 -> (c % 13 + 1).toString()
             10 -> "jack"
             11 -> "queen"
             12 -> "king"
             else -> "error"
         }
-
-//        return "c_${number}_of_${shape}"
         return if (c % 13 in 10..12)
             "c_${number}_of_${shape}2"
         else
             "c_${number}_of_${shape}"
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        Log.i("Lifecycle is started!!!!!!!", "onStart()")
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        Log.i("Lifecycle is started!!!!!!!", "onResume()")
-    }
-
-    override fun onPause() {
-        super.onPause()
-
-        Log.i("Lifecycle is started!!!!!!!", "onPause()")
-    }
-
-    override fun onStop() {
-        super.onStop()
-
-        Log.i("Lifecycle is started!!!!!!!", "onStop()")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-
-        Log.i("Lifecycle is started!!!!!!!", "onRestart()")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-
-        Log.i("Lifecycle is started!!!!!!!", "onDestroy()")
     }
 }
